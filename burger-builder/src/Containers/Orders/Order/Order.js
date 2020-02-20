c5b6226a7cada176ab8d49dd7c5eecb05f44c500
amith <amitkumarc@triconinfotech.com>
2020-02-20 18:17:20 +0530

    Working on orders page

---
+++ burger-builder/src/Containers/Orders/Order/Order.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Order.css';
+
+const Order = () => (
+    <div className={classes.Order}>
+        <p>Ingredients: Salad: 1</p>
+        <p>Price:7</p>
+    </div>
+);
+
+export default Order;
